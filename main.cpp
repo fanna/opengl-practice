d430af004053a7e6f582b2300ffd1d97e3b9b124
fanna <fanna993@gmail.com>
2015-10-17 19:32:28 +0200

    Fix main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
-// GLEW
-#define GLEW_STATIC
 #include <GL/glew.h>
-// GLFW
 #include <GLFW/glfw3.h>
 
+#define GLEW_STATIC
 
-// Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-//
+
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
-//
-// The MAIN function, from here we start the application and run the game loop
+
+// The main loop
 int main()
 {
   std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
-  // Init GLFW
+
   glfwInit();
-  // Set all the required options for GLFW
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-  // Create a GLFWwindow object that we can use for GLFW's functions
   GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
   if (window == nullptr)
   {
@@ -33,25 +29,23 @@ int main()
     return -1;
   }
   glfwMakeContextCurrent(window);
-  // Set the required callback functions
+
   glfwSetKeyCallback(window, key_callback);
 
-  // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
   glewExperimental = GL_TRUE;
-  // Initialize GLEW to setup the OpenGL Function pointers
+
   if (glewInit() != GLEW_OK)
   {
     std::cout << "Failed to initialize GLEW" << std::endl;
     return -1;
   }
 
-  // Define the viewport dimensions
+  //Viewport dimensions
   glViewport(0, 0, WIDTH, HEIGHT);
 
   // Game loop
   while (!glfwWindowShouldClose(window))
   {
-    // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
     glfwPollEvents();
 
     // Render
@@ -68,7 +62,6 @@ int main()
   return 0;
 }
 
-// Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
   std::cout << key << std::endl;
